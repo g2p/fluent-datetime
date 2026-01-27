60a8be27732c127cc2a26a7ed3e35c5ec7a2238a
Gabriel <g2p.code@gmail.com>
2026-01-27 18:25:24 +0000

    bench: Upgrade Criterion

--- benches/icu-formatter-instanciation.rs
+++ benches/icu-formatter-instanciation.rs
@@ -1,4 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, Criterion};
 use icu_calendar::{AnyCalendar, DateTime, Gregorian};
 use icu_datetime::options::length;
 use icu_datetime::{DateTimeFormatter, TypedDateTimeFormatter};
